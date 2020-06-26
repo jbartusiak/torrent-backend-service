d638a416fcb7f457a99efb6c82c3165950e94713
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-06-26 19:04:07 +0200

    barrel

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,21 @@ import TorrentSearch from 'torrent-search-api';
 import providersRouter from "./routes/providers";
 import torrentsRouter from "./routes/torrents";
 import cors from 'cors';
-import {corsOptions, configureDefaultProviders} from "./utils";
+import { configureDefaultProviders, corsOptions } from "./utils";
 import transmissionRouter from "./routes/transmission";
 
-process.env.APPLICATION_NAME='torrent-backend-service';
-process.env.APPLICATION_PORT='3001';
-process.env.DEFAULT_ENABLED_PROVIDERS='1337x,KickassTorrents';
+process.env.APPLICATION_NAME = 'torrent-backend-service';
+process.env.APPLICATION_PORT = '3001';
+process.env.DEFAULT_ENABLED_PROVIDERS = '1337x,KickassTorrents';
 
 const port = parseInt(process.env.APPLICATION_PORT);
 
 const app = Express();
 
-console.log(`Starting ${process.env.APPLICATION_NAME} on port: ${port}`)
+console.log(`Starting ${ process.env.APPLICATION_NAME } on port: ${ port }`)
 
-export {TorrentSearch as TorrentClient};
+export { TorrentSearch as TorrentClient };
+export * as Types from './types';
 
 app.use(cors(corsOptions));
 app.use(Express.json());
@@ -28,5 +29,5 @@ app.use(transmissionRouter);
 
 app.listen(port);
 
-console.log(`Configuring default providers: ${process.env.DEFAULT_ENABLED_PROVIDERS}`);
+console.log(`Configuring default providers: ${ process.env.DEFAULT_ENABLED_PROVIDERS }`);
 configureDefaultProviders();
